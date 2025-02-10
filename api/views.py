e5ae40ebf1f77f26f04845d35dbfa41446794dfe
Tiejun Hu <87230907+gzhutiejun@users.noreply.github.com>
2025-02-10 21:46:45 +0800

    update

--- api/views.py
+++ api/views.py
@@ -2,6 +2,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
-def getStatus(request):
-    status = {'app':'chatbot service', 'status':'ok', 'version':'0.1'}
-    return Response(status)
+def status(request):
+    result = {'app':'chatbot service', 'status':'ok', 'version':'0.1'}
+    return Response(result)
+
+@api_view(['POST', 'PUT'])
+def predict(request):
+    result = {'app':'chatbot service', 'status':'ok', 'version':'0.1'}
+    return Response(result)
